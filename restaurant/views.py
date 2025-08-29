e792b405b35931a74484a035d25c4f0c7e049d75
helmi mahmoud <hmhm61099@gmail.com>
2025-08-30 01:36:44 +0400

    feature: api creations

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,23 @@
-from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView, DestroyAPIView
+from rest_framework.viewsets import ModelViewSet
+from rest_framework import permissions
+from .models import Menu, Booking
+from .serializers import MenuSerializer, BookingSerializer, UserSerializer
+from django.contrib.auth.models import User
 
-# Create your views here.
-def index(request):
-    return render(request, 'index.html', {})
+class MenuItemsView(ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class SingleMenuItemView(RetrieveUpdateDestroyAPIView, DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class BookingsViewSet(ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+
+class UserViewSet(ModelViewSet):
+    # Assuming User model and UserSerializer are defined elsewhere
+    queryset = User.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
